bbc37f4b4013cd19c4073d0baf9057a58b8058fd
warshadi <warshadi@gmail.com>
2022-09-09 18:05:27 +0200

    create React Challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [apis, setapis] = useState({});
+  const [repos, setrepos] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.github.com/users/warshadi?client_id=01f66adcc33dcc4c7990&client_secret=7db31754076bcc4c7cf50dc8a33d12c09baad34f&sort=created"
+      )
+      .then((res) => {
+        setapis(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/warshadi/repos")
+      .then((repos) => setrepos(repos.data))
+      .catch((err) => console.log(err));
+  });
+  let imag = apis.avatar_url
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img src={imag} alt="person_photo"/>
+        <div className="details">
+          <p>FullName: <span>{apis.name}</span></p>
+          <p>UserName: <span>{apis.login}</span></p>
+          <p>Location: <span>{apis.location}</span></p>
+          <p>email: <span>{apis.email}</span></p>
+        </div>
+      </div>
+      <h2>User Repos</h2>
+      <ul className="repo-name">
+        {repos.map((item) => {
+          return <li key={item.id}>
+            <a href={'https://github.com/' + item.full_name}>{item.name}</a>
+            </li>;
+        })}
+      </ul>
     </div>
   );
 }
-
 export default App;
